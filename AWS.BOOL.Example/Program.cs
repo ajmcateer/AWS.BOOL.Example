9b1bccece510b80208018bee264327f4afa2dbc4
ajmcateer <ajmcateer@gmail.com>
2021-04-17 14:35:01 -0400

    Initial Commit

---
+++ AWS.BOOL.Example/Program.cs
@@ -0,0 +1,27 @@
+﻿using Amazon.DynamoDBv2.Model;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Text;
+
+namespace AWS.BOOL.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var streamReader = new StreamReader("example.json"))
+            {
+                string reader = streamReader.ReadToEnd();
+                Console.WriteLine("AttributeValue before being de-serialized");
+                Console.WriteLine(reader);
+
+                var result = JsonConvert.DeserializeObject<AttributeValue>(reader);
+
+                Console.WriteLine("AttributeValue after being de-serialized");
+                Console.WriteLine($@"Bool Value: {result.BOOL}");
+            }
+        }
+    }
+}
+
